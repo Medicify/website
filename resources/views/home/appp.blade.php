353752b9440bc7a7d6edb30eea2f9c5bc1ad38e7
bharaayongpm <ayongbhara01@gmail.com>
2023-12-21 15:02:23 +0700

    update

---
+++ resources/views/home/appp.blade.php
@@ -0,0 +1,215 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- ========== Meta Tags ========== -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Appku - Software Landing Page">
+
+        <!-- ========== Page Title ========== -->
+        <title>Medicify konten</title>
+
+        <!-- ========== Favicon Icon ========== -->
+        <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/x-icon">
+
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/helper.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/validnavs.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/flaticon-set.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/elegant-icons.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+        {{-- <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+        <link href="assets/css/themify-icons.css" rel="stylesheet" />
+        <link href="assets/css/elegant-icons.css" rel="stylesheet" />
+        <link href="assets/css/flaticon-set.css" rel="stylesheet" />
+        <link href="assets/css/magnific-popup.css" rel="stylesheet" />
+        <link href="assets/css/owl.carousel.min.css" rel="stylesheet" />
+        <link href="assets/css/owl.theme.default.min.css" rel="stylesheet" />
+        <link href="assets/css/animate.css" rel="stylesheet" />
+        <link href="assets/css/validnavs.css" rel="stylesheet" />
+        <link href="assets/css/helper.css" rel="stylesheet" />
+        <link href="assets/css/style.css" rel="stylesheet" />
+        <link href="style.css" rel="stylesheet">
+        <link href="assets/css/responsive.css" rel="stylesheet" /> --}}
+<body>
+
+    <!-- Preloader Start -->
+
+      <!-- Preloader Ends -->
+
+    <!-- End Header -->
+
+
+                  @yield('content')
+
+
+
+<!-- Start Footer
+    ============================================= -->
+    <footer class="bg-dark text-light">
+        <div class="container">
+            <div class="f-items default-padding">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 item">
+                        <div class="f-item about">
+                            <img src="assets/img/logo_putih.png" alt="Logo">
+                            <p>
+                                Bridging Hope and Healing: Medicify, Your Visionary Guide to Medications
+                            </p>
+                            <form action="#">
+                                <input type="email" placeholder="Your Email" class="form-control" name="email">
+                                <button type="submit"> <i class="arrow_right"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-6 item">
+                        <div class="f-item link">
+                            <h4 class="widget-title">Quick LInk</h4>
+                            <ul>
+                                <li>
+                                    <a href="index.html"><i class="fas fa-angle-right"></i> Home</a>
+                                </li>
+                                <li>
+                                    <a href="about-us.html"><i class="fas fa-angle-right"></i> About us</a>
+                                </li>
+                                <li>
+                                    <a href="about-us.html"><i class="fas fa-angle-right"></i> Compnay History</a>
+                                </li>
+                                <li>
+                                    <a href="services.html"><i class="fas fa-angle-right"></i> Features</a>
+                                </li>
+                                <li>
+                                    <a href="blog-with-sidebar.html"><i class="fas fa-angle-right"></i> Blog Page</a>
+                                </li>
+                                <li>
+                                    <a href="about-us.html"><i class="fas fa-angle-right"></i> Login</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 item">
+                        <div class="f-item link">
+                            <h4 class="widget-title">Community</h4>
+                            <ul>
+                                <li>
+                                    <a href="services-details.html"><i class="fas fa-angle-right"></i> Career</a>
+                                </li>
+                                <li>
+                                    <a href="services-details.html"><i class="fas fa-angle-right"></i> Leadership</a>
+                                </li>
+                                <li>
+                                    <a href="services-details.html"><i class="fas fa-angle-right"></i> Strategy</a>
+                                </li>
+                                <li>
+                                    <a href="services.html"><i class="fas fa-angle-right"></i> Services</a>
+                                </li>
+                                <li>
+                                    <a href="about-us.html"><i class="fas fa-angle-right"></i> History</a>
+                                </li>
+                                <li>
+                                    <a href="services-details.html"><i class="fas fa-angle-right"></i> Components</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 item">
+                        <div class="f-item contact-widget">
+                            <h4 class="widget-title">Contact Info</h4>
+                            <div class="address">
+                                <ul>
+                                    <li>
+                                        <div class="icon">
+                                            <i class="fas fa-home"></i>
+                                        </div>
+                                        <div class="content">
+                                            <strong>Address:</strong>
+                                            5919 Trussville Crossings Pkwy, Birmingham
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="icon">
+                                            <i class="fas fa-envelope"></i>
+                                        </div>
+                                        <div class="content">
+                                            <strong>Email:</strong>
+                                            <a href="mailto:info@validtheme.com">info@validtheme.com</a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="icon">
+                                            <i class="fas fa-phone"></i>
+                                        </div>
+                                        <div class="content">
+                                            <strong>Phone:</strong>
+                                            <a href="tel:2151234567">+123 34598768</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Start Footer Bottom -->
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <p>&copy; Copyright 2023. All Rights Reserved by <a href="https://themeforest.net/user/validthemes/portfolio">Medicify</a></p>
+                    </div>
+                    <div class="col-lg-6 text-end link">
+                        <ul>
+                            <li>
+                                <a href="about-us.html">Terms</a>
+                            </li>
+                            <li>
+                                <a href="about-us.html">Privacy</a>
+                            </li>
+                            <li>
+                                <a href="contact.html">Support</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Footer Bottom -->
+    </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+
+</body>
+<script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('assets/js/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.appear.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('assets/js/modernizr.custom.13711.js') }}"></script>
+<script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('assets/js/wow.min.js') }}"></script>
+<script src="{{ asset('assets/js/progress-bar.min.js') }}"></script>
+<script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets/js/count-to.js') }}"></script>
+<script src="{{ asset('assets/js/YTPlayer.min.js') }}"></script>
+<script src="{{ asset('assets/js/validnavs.js') }}"></script>
+<script src="{{ asset('assets/js/main.js') }}"></script>
+
+</html>
